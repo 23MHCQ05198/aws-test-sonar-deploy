ed232b374e199c4534b159dd4cd4969e5a7c3f59
Ramya Tamarana <r3411707@gmail.com>
2026-02-28 11:40:31 +0530

    commit

---
+++ src/main/java/com/example/demo/AwsTestingDeploysApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsTestingDeploysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsTestingDeploysApplication.class, args);
+	}
+
+}
